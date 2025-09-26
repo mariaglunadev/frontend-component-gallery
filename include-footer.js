1f19d7d393811a1e644b9e30c53d3c834f32543e
MariaGonzalez <dgmariagonzalez@gmail.com>
2025-09-26 11:06:25 -0500

    Test global footer

---
+++ include-footer.js
@@ -0,0 +1,21 @@
+// include-footer.js
+document.addEventListener('DOMContentLoaded', () => {
+    const footerPath = '/00-assets/footer.html'; 
+
+    fetch(footerPath)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al cargar el footer: ' + response.statusText);
+            }
+            return response.text();
+        })
+        .then(htmlContent => {
+            const footerElement = document.createElement('footer');
+            footerElement.innerHTML = htmlContent;
+
+            document.body.appendChild(footerElement);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
